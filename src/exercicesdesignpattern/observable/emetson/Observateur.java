69e807ee86ba142e7a0977d2ea341d77b72a1413
Thomas SAUVAJON <thomas.sauvajon@gmail.com>
2017-05-31 14:43:37 +0200

    observable

--- src/exercicesdesignpattern/observable/emetson/Observateur.java
+++ src/exercicesdesignpattern/observable/emetson/Observateur.java
@@ -3,12 +3,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package exercicesdesignpattern.emetson;
+package exercicesdesignpattern.observable.emetson;
 
 /**
  *
  * @author thomas.sauvajon
  */
-public class Oie {
-    
+public class Observateur {
+    public void reagit (Observable obs) {
+        System.out.println("Je réagis !");
+    }
 }
